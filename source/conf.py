986bd30ad5afd7a679ad6dd793117b8127347e23
Michael Obikwere <oobikwere@academic.rrc.ca>
2025-11-20 16:52:56 -0600

    3rd Commit: Added some imports and an extension to the conf.py file.

--- source/conf.py
+++ source/conf.py
@@ -5,6 +5,10 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 
 project = 'Sports Application'
 copyright = '2025, Michael Obikwere'
@@ -14,7 +18,9 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
